316a7938d5d5baf88f9a0e543f00e18a9e4b6c62
David Oberlander <david.oberlander@student.cvtc.edu>
2022-04-28 01:32:34 -0500

    [Delivers #181880056] Add slideshow images to home

---
+++ js/slideshow.js
@@ -0,0 +1,32 @@
+let slideInt = 1;
+let nextSlide = 2;
+
+function startSlideshow() {
+  // show the first image immediately
+  $('#slide-img-' + slideInt).show();
+
+  // Get count of all the images inside the slideshow
+  let count = $('#slideshow img').length;
+  setInterval(function() {
+      // Fade out the last image
+      $("#slide-img-" + slideInt).fadeOut(500);
+      // Fade in the next image
+      $("#slide-img-" + nextSlide).fadeIn(1000);
+      slideInt++;
+      nextSlide++;
+
+      // Reset if end of the images
+      if(nextSlide > count) {
+        nextSlide = 1;
+      }
+      if(slideInt > count) {
+        slideInt = 1;
+      }
+  }, 5000);
+}
+
+$(function() {
+  // Hide all the slideshow images initially
+  $('#slideshow img').hide();
+  startSlideshow();
+});
